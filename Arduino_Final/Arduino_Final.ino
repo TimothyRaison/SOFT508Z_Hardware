bf6dc40ae1fb639cbd1c388d7b8669734edb951d
TimothyRaison <74619586+TimothyRaison@users.noreply.github.com>
2021-01-21 01:49:30 +0000

    Add files via upload

---
+++ Arduino_Final/Arduino_Final.ino
@@ -0,0 +1,17 @@
+// This code is the final scetch for Arduino Mega which includes:
+// Communicating with the ESP32 board using SPI (full duplex)
+// Interfacing with the motors on the buggy using the motor sheild mounted on the Arduino board
+// Using a ultrasconic sensor to measure distance (in front of the buggy)
+
+void setup() {
+
+
+
+
+}
+
+void loop() {
+
+
+
+}
